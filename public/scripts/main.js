9e1f2c1900b81a94121082e843b1624758f9396c
Jean Rufino <jeanbruno1991@gmail.com>
2021-07-06 17:40:22 -0300

    touch project test

---
+++ public/scripts/main.js
@@ -0,0 +1,15 @@
+const project = document.querySelector(".project-wrapper");
+console.log(project);
+
+function touch() {
+    project.classList.add('active');
+    console.log('Confirmed touch!')
+}
+
+function untouch() {
+    project.classList.remove('active');
+    console.log('Confirmed untouch!')
+}
+
+project.addEventListener('touchend', untouch);
+project.addEventListener('touchstart', touch);
